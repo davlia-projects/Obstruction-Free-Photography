c6420ff255b468f81e1b7e220900047c172f5ce7
David Liao <danedaworld@gmail.com>
2016-12-09 17:33:20 -0500

    fixed bug with negative numbers

--- src/canny.cpp
+++ src/canny.cpp
@@ -74,7 +74,9 @@ void kernConvolution(int N, unsigned char * in, unsigned char * out, int width, 
     			}
     		}
     	}
-    	out[y * width + x] = c;
+    	out[y * width + x] = fabs(c);
+      // if (c > 254 || c < 0)
+      //   printf("%f %d\n", c, out[y * width + x]);
     }
   }
 }
@@ -84,27 +86,26 @@ void kernGradient(int N, unsigned char * Gx, unsigned char * Gy, unsigned char *
   for (int i = 0; i < N; ++i) {
     gradient[i] = sqrt(Gx[i] * Gx[i] + Gy[i] * Gy[i]); // this might need to be a float
     angle = (atan2(Gx[i], Gy[i]) / 3.14159f) * 180.0f;
-    if (((angle < 22.5) && (angle > -22.5)) || (angle > 157.5) || (angle < -157.5)) {
+    if (((-22.5 < angle) && (angle <= 22.5)) || ((157.5 < angle) && (angle <= -157.5))) {
       roundedAngle = 0;
     }
-		if (((angle > 22.5) && (angle < 67.5)) || ((angle < -112.5) && (angle > -157.5))) {
+		if (((-157.5 < angle) && (angle <= -112.5)) || ((22.5 < angle) && (angle <= 67.5))) {
       roundedAngle = 45;
     }
-		if (((angle > 67.5) && (angle < 112.5)) || ((angle < -67.5) && (angle > -112.5))) {
+		if (((-112.5 < angle) && (angle <= -67.5)) || ((67.5 < angle) && (angle <= 112.5))) {
       roundedAngle = 90;
     }
-		if (((angle > 112.5) && (angle < 157.5)) || ((angle < -22.5) && (angle > -67.5))) {
+		if (((-67.5 < angle) && (angle <= -22.5)) || ((112.5 < angle) && (angle <= 157.5))) {
       roundedAngle = 135;
     }
     edgeDir[i] = roundedAngle;
   }
 }
 
-void findEdge(unsigned char * gradient, unsigned char * edgeDir, unsigned char * out, int rowShift, int colShift, int row, int col, int dir, int width, int height) {
+void findEdge(unsigned char * gradient, unsigned char * edgeDir, unsigned char * out, int rowShift, int colShift, int row, int col, int dir, int width, int height, bool & edgeEnd) {
   int newRow;
   int newCol;
   unsigned long i;
-  bool edgeEnd = false;
   /* Find the row and column values for the next possible pixel on the edge */
 	if (colShift < 0) {
 		if (col > 0) {
@@ -164,16 +165,16 @@ void edgeTrace(int N, unsigned char * gradient, unsigned char * edgeDir, unsigne
       if (gradient[i * width + j] > UPPERTHRESHOLD) {
         switch(edgeDir[i * width + j]) {
           case 0:
-            findEdge(gradient, edgeDir, out, 0, 1, i, j, 0, width, height);
+            findEdge(gradient, edgeDir, out, 0, 1, i, j, 0, width, height, edgeEnd);
             break;
           case 45:
-            findEdge(gradient, edgeDir, out, 1, 1, i, j, 45, width, height);
+            findEdge(gradient, edgeDir, out, 1, 1, i, j, 45, width, height, edgeEnd);
             break;
           case 90:
-            findEdge(gradient, edgeDir, out, 1, 0, i, j, 90, width, height);
+            findEdge(gradient, edgeDir, out, 1, 0, i, j, 90, width, height, edgeEnd);
             break;
           case 135:
-            findEdge(gradient, edgeDir, out, 1, -1, i, j, 135, width, height);
+            findEdge(gradient, edgeDir, out, 1, -1, i, j, 135, width, height, edgeEnd);
             break;
           default:
             out[i * width + j] = 0;
@@ -193,6 +194,128 @@ void edgeTrace(int N, unsigned char * gradient, unsigned char * edgeDir, unsigne
 }
 
 
+// void nonMaxSuppression(int N, unsigned char * in, unsigned char * out, int width, int height) {
+//   for (int i = 1; i < height - 1; ++i) {
+//     for (int j = 1; j < width - 1; ++j) {
+//       int angle = in[i * width + j];
+//       switch(angle) {
+//         case 0:
+//           if (in[i * width + j] < in[i * width + j + 1] || in[i * width + j] < in[i * width + j - 1]) {
+//             out[(i - 1) * width + j - 1] = 0;
+//           }
+//           break;
+//         case 45:
+//           if (in[i * width + j] < in[(i + 1) * width + j + 1] || in[i * width + j] < in[(i - 1) * width + j - 1]) {
+//             out[(i - 1) * width + j - 1] = 0;
+//           }
+//           break;
+//         case 90:
+//           if (in[i * width + j] < in[(i + 1) * width + j] || in[i * width + j] < in[(i - 1) * width + j]) {
+//             out[(i - 1) * width + j - 1] = 0;
+//           }
+//
+//           break;
+//         case 135:
+//           if (in[i * width + j] < in[(i + 1) * width + j - 1] || in[i * width + j] < in[(i - 1) * width + j + 1]) {
+//             out[(i - 1) * width + j - 1] = 0;
+//           }
+//           break;
+//         default:
+//           break;
+//       }
+//     }
+//   }
+// }
+
+void nonMaxSuppression(int N, unsigned char * in, unsigned char * out, int width, int height) {
+  for (int i = 1; i < height - 1; ++i) {
+    for (int j = 1; j < width - 1; ++j) {
+      int angle = in[i * width + j];
+      switch(angle) {
+        case 0:
+          if (in[i * width + j] < in[i * width + j + 1] || in[i * width + j] < in[i * width + j - 1]) {
+            out[(i - 1) * width + j - 1] = 0;
+          }
+          break;
+        case 45:
+          if (in[i * width + j] < in[(i + 1) * width + j + 1] || in[i * width + j] < in[(i - 1) * width + j - 1]) {
+            out[(i - 1) * width + j - 1] = 0;
+          }
+          break;
+        case 90:
+          if (in[i * width + j] < in[(i + 1) * width + j] || in[i * width + j] < in[(i - 1) * width + j]) {
+            out[(i - 1) * width + j - 1] = 0;
+          }
+
+          break;
+        case 135:
+          if (in[i * width + j] < in[(i + 1) * width + j - 1] || in[i * width + j] < in[(i - 1) * width + j + 1]) {
+            out[(i - 1) * width + j - 1] = 0;
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
+}
+
+void hysteresis(int N, unsigned char * in, unsigned char * out, int width, int height) {
+  for (int i = 0; i < height; i++) {
+    for (int j = 0; i < width; j++) {
+      int idx = i * width + j;
+      out[idx] = in[idx];
+      if (out[idx] > UPPERTHRESHOLD) {
+        out[idx] = 255;
+      } else if (out[idx] < LOWERTHRESHOLD) {
+        out[idx] = 0;
+      } else {
+        bool anyHigh = false;
+        bool anyBetween = false;
+        for (int x = i - 1; x <= i + 1; ++x) {
+          for (int y = j - 1; y <= j + 1; ++y) {
+            if (x < 0 || y < 0 || x >= width || y >= height) {
+              continue;
+            }
+            int xidx = x * width + y;
+            if (out[xidx] > UPPERTHRESHOLD) {
+              out[idx] = 255;
+              anyHigh = true;
+              break;
+            } else if (out[xidx] <= UPPERTHRESHOLD && out[xidx] >= LOWERTHRESHOLD) {
+              anyBetween = true;
+            }
+          }
+          if (anyHigh) {
+            break;
+          }
+        }
+        if (!anyHigh && anyBetween) {
+          for (int x = i - 2; x <= i + 2; x++) {
+            for (int y = j - 2; j <= j + 2; j++) {
+              if (x < 0 || y < 0 || x >= width || y >= height) {
+                continue;
+              }
+              int xidx = x * width + y;
+              if (out[xidx] > UPPERTHRESHOLD) {
+                out[idx] = 255;
+                anyHigh = true;
+                break;
+              }
+            }
+            if (anyHigh) {
+              break;
+            }
+          }
+          if(!anyHigh) {
+            out[idx] = 0;
+          }
+        }
+      }
+    }
+  }
+}
+
 void stride(int N, unsigned char * in, unsigned char * out) {
   for (int i = 0; i < N; i += 3) {
     out[i] = in[i / 3];
@@ -203,24 +326,23 @@ void stride(int N, unsigned char * in, unsigned char * out) {
 
 unsigned char * Canny::edge(int N, unsigned char * in, int width, int height) {
   unsigned char * grayscale = new unsigned char[N / 3];
-  kernGrayscale(N, in, grayscale);
-  unsigned char * blur = new unsigned char[N / 3];
-  kernConvolution(N / 3, grayscale, blur, width, height, gaussian, 5);
-
+  unsigned char * smooth = new unsigned char[N / 3];
   unsigned char * Gx_mask = new unsigned char[N / 3];
-  kernConvolution(N / 3, blur, Gx_mask, width, height, G_x, 3);
-
   unsigned char * Gy_mask = new unsigned char[N / 3];
-  kernConvolution(N / 3, blur, Gy_mask, width, height, G_y, 3);
-
   unsigned char * gradient = new unsigned char[N / 3];
   unsigned char * edgeDir = new unsigned char[N / 3];
-  kernGradient(N / 3, Gx_mask, Gy_mask, gradient, edgeDir);
 
+  kernGrayscale(N, in, grayscale);
+  kernConvolution(N / 3, grayscale, smooth, width, height, gaussian, 5);
+  kernConvolution(N / 3, smooth, Gx_mask, width, height, G_x, 3);
+  kernConvolution(N / 3, smooth, Gy_mask, width, height, G_y, 3);
+  kernGradient(N / 3, Gx_mask, Gy_mask, gradient, edgeDir);
   edgeTrace(N / 3, gradient, edgeDir, grayscale, width, height);
+  // nonMaxSuppression(N / 3, edgeDir, grayscale, width, height);
+  // hysteresis(N / 3, grayscale, smooth, width, height);
 
   unsigned char * out = new unsigned char[N];
-  stride(N, Gx_mask, out);
+  stride(N, grayscale, out);
 
   return out;
 }
