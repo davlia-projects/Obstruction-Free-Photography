395ca20eb7a2d683b0edfd4dcca493ad6c2a41bc
Zhan Xiong Chin <chinz@seas.upenn.edu>
2016-12-15 06:23:54 +0000

    fix header

--- src/canny.h
+++ src/canny.h
@@ -2,5 +2,5 @@
 
 namespace Canny {
   unsigned char * edge(int N, int width, int height, unsigned char * in);
-  void kernSmooth(int N, int width, int height, unsigned char * in, unsigned char * out, const float * kernel, int kernSize);
+  // void kernSmooth(int N, int width, int height, unsigned char * in, unsigned char * out, const float * kernel, int kernSize);
 }
